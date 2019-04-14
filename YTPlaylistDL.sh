6971e2224c74c8d062e09edebef03ea82bc89a2c
Vincenzo Marcella <vima892@gmail.com>
2019-04-14 17:04:21 +0200

    Delete YTPlaylistDL.sh

--- YTPlaylistDL.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-read -p "Insert link of playlist: " link
-read -p "Insert the path of the storage folder(ex. /home/user/Music): " path1
-path1=$path1"/%(title)s.%(ext)s"
-echo $path1
-youtube-dl -xi --audio-format flac --audio-quality 0 --output $path1 --yes-playlist $link 
-echo "Done!"
